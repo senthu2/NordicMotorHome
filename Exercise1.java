ebdd9d2170852ec7f039a7aa72eff2fc55b83682
senthu2 <56972476+senthu2@users.noreply.github.com>
2020-05-19 17:27:53 +0200

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+
+    public class Exercise1 {
+        public static void main(String[] args) {
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < 10; i++) {
+                j++;
+                System.out.println(i + " + " + j + " = " + (i+j));
+            }
+            System.out.println("I’ve printed out the sums of i and j up to bob12 809"
+                    + i + "," + j);
+        }
+    }
+
+
